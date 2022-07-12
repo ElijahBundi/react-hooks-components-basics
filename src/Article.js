16adcd482559f910475479d840afa7b0ce9f7ca7
Elijah <bundielly@gmail.com>
2022-07-12 20:31:07 +0300

    assignment complete

--- src/Article.js
+++ src/Article.js
@@ -6,4 +6,10 @@ function Article() {
   );
 }
 
+// class Article extends React.component {
+//   render() {
+//     return <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+//   }
+// }
+
 export default Article;
